57d8ae5f405f861f2eea6630c7524603d9c438a7
Aleksandar Stojkov <a.stojkv5@gmail.com>
2025-06-27 14:22:09 +0200

    Footer Changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,6 @@ import {
 import Logo from "/FinRule.png";
 import "../styles/Footer.css";
 
-// Framer Motion variants
 const container = {
   hidden: {},
   visible: {
@@ -32,121 +31,119 @@ const fadeIn = {
 
 const Footer = () => {
   const { t } = useTranslation();
-  return (
-    <motion.footer
-      initial="hidden"
-      whileInView="visible"
-      variants={container}
-      viewport={{ once: false, amount: 0.2 }}
-      className="relative text-white mt-10 mb-10 overflow-hidden"
-    >
-      {/* Footer Container */}
-      <Row justify="center" className="w-full px-4 ">
-        <Col
-          xs={24}
-          md={20}
-          className="bg-blue-100/10 p-4 rounded backdrop-blur-md  relative"
-        >
-          {/* Mobile background logo */}
-          <div className="md:hidden absolute inset-0 z-[-1] opacity-50 flex items-center justify-center">
-            <img src={Logo} alt="FinRule Logo" className="w-2/3" />
-          </div>
 
-          <Row
-            gutter={[24, 24]}
-            justify="space-between"
-            align="middle"
-            className="text-center md:text-left"
+  return (
+    <>
+      {/* Main Footer Section with Background */}
+      <motion.footer
+        initial="hidden"
+        whileInView="visible"
+        variants={container}
+        viewport={{ once: false, amount: 0.2 }}
+        className="relative text-white mt-10 overflow-hidden"
+      >
+        {/* Background logo */}
+        <div className="footer-bg-logo">
+          <img src={Logo} alt="FinRule Logo" />
+        </div>
+
+        <Row justify="center" className="w-full  relative z-10">
+          <Col
+            md={20}
+            xs={22}
+            className="bg-blue-100/10 p-6 md:p-8 rounded-2xl backdrop-blur-md relative"
           >
-            {/* Logo (desktop) */}
-            <Col xs={24} md={6}>
-              <div className="hidden md:flex flex-col items-center md:items-start">
+            <Row
+              gutter={[16, 16]}
+              justify="space-between"
+              align="middle"
+              className="text-center md:text-left"
+            >
+              <Col span={24}>
+                <motion.h4
+                  className="font-semibold footer-links text-center"
+                  variants={fadeIn}
+                >
+                  {t("footer.quick")}
+                </motion.h4>
+              </Col>
+              <Col span={24}>
+                <ul className="flex gap-3 text-sm footer-links text-center justify-center items-center flex-wrap">
+                  <motion.li variants={fadeIn}>
+                    <NavLink to="/" className="footer-link">
+                      <Home className="inline w-4 h-4 mr-2" />
+                      {t("footer.home")}
+                    </NavLink>
+                  </motion.li>
+                  <motion.li variants={fadeIn}>
+                    <NavLink to="/about" className="footer-link">
+                      <Info className="inline w-4 h-4 mr-2" />
+                      {t("footer.about")}
+                    </NavLink>
+                  </motion.li>
+                  <motion.li variants={fadeIn}>
+                    <NavLink to="/product" className="footer-link">
+                      <ShoppingBag className="inline w-4 h-4 mr-2" />
+                      {t("footer.product")}
+                    </NavLink>
+                  </motion.li>
+                  <motion.li variants={fadeIn}>
+                    <NavLink to="/solutions" className="footer-link">
+                      <Layers className="inline w-4 h-4 mr-2" />
+                      {t("footer.solutions")}
+                    </NavLink>
+                  </motion.li>
+                  <motion.li variants={fadeIn}>
+                    <NavLink to="/contact" className="footer-link">
+                      <Mail className="inline w-4 h-4 mr-2" />
+                      {t("footer.contact")}
+                    </NavLink>
+                  </motion.li>
+                </ul>
+              </Col>
+
+              {/* Legal Note */}
+              <Col span={24} className="text-center text-md text-gray-400">
+                FinRule M7 LLC
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </motion.footer>
+
+      {/* Footer Bottom (separate from main background) */}
+      <Row justify="center" className=" ">
+        <Col span={20}>
+          <Row justify="center" align="middle">
+            <Col xs={24} md={12}>
+              <div className="hidden md:flex items-center justify-start mb-2 md:mb-0">
                 <img
-                  src={Logo}
-                  alt="FinRule Logo"
-                  className="h-full mb-4 animate-pulse max-h-24"
+                  src="/FinRule.png"
+                  className="w-20"
+                  alt="FinRule Logo Mini"
                 />
               </div>
             </Col>
-
-            {/* Quick Links */}
-            <Col xs={24} md={8}>
-              <motion.h4
-                className="font-semibold mb-2 footer-links text-center md:text-left"
-                variants={fadeIn}
-              >
-                {t("footer.quick")}
-              </motion.h4>
-              <ul className="space-y-1 text-sm footer-links text-center md:text-left">
-                <motion.li variants={fadeIn}>
-                  <NavLink to="/" className="footer-link">
-                    <Home className="inline w-4 h-4 mr-2" />
-                    {t("footer.home")}
-                  </NavLink>
-                </motion.li>
-                <motion.li variants={fadeIn}>
-                  <NavLink to="/about" className="footer-link">
-                    <Info className="inline w-4 h-4 mr-2" />
-                    {t("footer.about")}
-                  </NavLink>
-                </motion.li>
-                <motion.li variants={fadeIn}>
-                  <NavLink to="/product" className="footer-link">
-                    <ShoppingBag className="inline w-4 h-4 mr-2" />
-                    {t("footer.product")}
-                  </NavLink>
-                </motion.li>
-                <motion.li variants={fadeIn}>
-                  <NavLink to="/solutions" className="footer-link">
-                    <Layers className="inline w-4 h-4 mr-2" />
-                    {t("footer.solutions")}
-                  </NavLink>
-                </motion.li>
-                <motion.li variants={fadeIn}>
-                  <NavLink to="/contact" className="footer-link">
-                    <Mail className="inline w-4 h-4 mr-2" />
-                    {t("footer.contact")}
-                  </NavLink>
-                </motion.li>
-              </ul>
-            </Col>
-
-            {/* Legal */}
-            <Col xs={24} md={8}>
-              <motion.h4
-                className="font-semibold mb-2 footer-links text-center md:text-left"
-                variants={fadeIn}
-              >
-                {t("footer.legal")}
-              </motion.h4>
-              <ul className="space-y-1 text-sm footer-links text-center md:text-left">
-                <motion.li variants={fadeIn}>
+            <Col xs={24} md={12}>
+              <ul className="flex gap-3 justify-end items-center text-sm footer-links text-end">
+                <li>
                   <NavLink to="/privacy" className="footer-link">
                     <Shield className="inline w-4 h-4 mr-2" />
                     {t("footer.privacy")}
                   </NavLink>
-                </motion.li>
-                <motion.li variants={fadeIn}>
+                </li>
+                <li>
                   <NavLink to="/terms" className="footer-link">
                     <FileText className="inline w-4 h-4 mr-2" />
                     {t("footer.terms")}
                   </NavLink>
-                </motion.li>
+                </li>
               </ul>
             </Col>
           </Row>
         </Col>
       </Row>
-
-      {/* Footer note */}
-      <motion.div variants={fadeIn}>
-        <Row justify="center" className="mt-5">
-          <Col span={20} className="text-center text-md text-gray-500">
-            FinRule M7 LLC
-          </Col>
-        </Row>
-      </motion.div>
-    </motion.footer>
+    </>
   );
 };
 
